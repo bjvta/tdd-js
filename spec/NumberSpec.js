bd8b5bcdd92a48276c3ef749f0c058c7d1765f1c
Darlyn <bpdarlyn@gmail.com>
2022-03-16 18:27:53 -0400

    working with local libraries and use only browser

---
+++ spec/NumberSpec.js
@@ -0,0 +1,8 @@
+describe("Number", function() {
+  it("should get the highest number", function() {
+    let number = new Number();
+    let highest = number.highest_number(1,2,3)
+    let expected = 3
+    expect(highest).toEqual(expected);
+  });
+});
---
+++ src/Number.js
@@ -0,0 +1,5 @@
+// function Number() {
+// }
+// Number.prototype.highest_number = function(n1, n2, n3) {
+//   return null
+// }
