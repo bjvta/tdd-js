33a3e07fd2aa76e99d327d65d457d54be5108e57
Brandon Valle <bjvtamayo78@gmail.com>
2022-03-18 17:27:18 -0400

    test: add more tests

--- spec/NumberSpec.js
+++ spec/NumberSpec.js
@@ -1,10 +1,30 @@
 describe("Number", function() {
   it("should get the highest number", function() {
-    let number = new Number();
+    let number = new Numbers();
     let highest = number.highest_number(1,2,3)
     let expected = 3
     expect(highest).toEqual(expected);
   });
 
+  it("should get the highest number with similar number", function(){
+    let number = new Numbers();
+    let highest = number.highest_number(-1, 1, 1)
+    expected = 1;
+    expect(highest).toEqual(expected);
+  })
 
+  it("should get a hightes number from two numbers", function(){
+    let number = new Numbers();
+    let highest = number.highest_number_two(1, 2);
+    expected = 2;
+    expect(highest).toEqual(expected);
+  })
+
+  it("should get a hightes number from args", function(){
+    let number = new Numbers();
+    let highest = number.highest_number(1, 2, 3, 4, 5);
+    expected = 5;
+    expect(highest).toEqual(expected);
+  })
+  
 });
--- src/Number.js
+++ src/Number.js
@@ -1,6 +1,20 @@
-// function Number() {
-// }
-//
-// Number.prototype.highest_number = function() {
-//     return null;
-// }
+function Numbers() {
+}
+
+
+
+
+Numbers.prototype.highest_number = function() {
+	let highestNumber = arguments[0];
+	for(let i=1; i< arguments.length; i++) {
+		highestNumber = this.highest_number_two(highestNumber, arguments[i]);
+	}
+	return highestNumber
+}
+
+Numbers.prototype.highest_number_two = function(n1, n2) {
+	if (n1 > n2) {
+		return n1;
+	}
+	return n2;
+}
